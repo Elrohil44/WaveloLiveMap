d498734dbfb9c46cc979e1d4065d06a8f6eb7374
Elrohil44 <w.stanek123@gmail.com>
2017-06-29 21:13:12 +0200

    Make this nicer

--- build.sbt
+++ build.sbt
@@ -1,17 +1,12 @@
 name := "WaveloLiveMap"
-import com.lihaoyi.workbench.Plugin._
 
-workbenchSettings
-
-bootSnippet := "example.T1().main(document.getElementById('mydiv'));"
-libraryDependencies += "com.lihaoyi" %%% "upickle" % "0.4.3"
-libraryDependencies += "fr.hmil" %%% "roshttp" % "2.0.1"
-
+//bootSnippet := "example.T1().main(document.getElementById('mydiv'));"
 resolvers += "jitpack" at "https://jitpack.io"
 
-libraryDependencies += "com.github.fancellu.scalajs-leaflet" % "scalajs-leaflet_sjs0.6_2.11" % "v0.1"
+libraryDependencies += "com.lihaoyi" %%% "upickle" % "0.4.3"
+libraryDependencies += "fr.hmil" %%% "roshttp" % "2.0.1"
+libraryDependencies += "com.github.fancellu.scalajs-leaflet" %%% "scalajs-leaflet" % "v0.1"
 
 enablePlugins(ScalaJSPlugin)
-publishArtifact:= false
 scalaVersion in ThisBuild := "2.11.8"
     
--- src/main/scala/Map.scala
+++ src/main/scala/Map.scala
@@ -1,10 +1,12 @@
-import com.felstar.scalajs.leaflet.{IconOptions, L, LMap, LMapOptions, Marker, MarkerOptions, TileLayerOptions}
+import com.felstar.scalajs.leaflet.{ControlLayerOptions, Icon, IconOptions, L, LMap, LMapOptions, LayerGroup, Marker, MarkerOptions, TileLayerOptions}
 import fr.hmil.roshttp.HttpRequest
 import fr.hmil.roshttp.Protocol.HTTP
 import fr.hmil.roshttp.response.SimpleHttpResponse
 import monix.execution.Scheduler.Implicits.global
 
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.annotation.{JSExport, JSExportDescendentObjects, JSExportTopLevel}
+import scala.scalajs.js.Dynamic.literal
+import scala.scalajs.reflect.Reflect.lookupLoadableModuleClass
 import scala.util.{Failure, Success}
 import upickle._
 
@@ -12,11 +14,6 @@ import scala.collection.immutable.{HashMap, HashSet}
 
 
 class Bike(val id:Int, val latitude: Double, val longitude: Double){
-
-  def equal(other: Bike): Boolean = {
-    other.id == id && other.latitude == latitude && other.longitude == longitude
-  }
-
   override def equals(other: Any): Boolean = other match {
     case that: Bike => (that canEqual this) && (id == that.id)
     case _ => false
@@ -33,84 +30,130 @@ object Bike{
   }
   implicit val thing2Reader = upickle.default.Reader[Bike]{
     case Js.Obj(("id", id), ("latitude", lat), ("longitude", lon)) =>
-      new Bike(id.asInstanceOf[Int], lat.asInstanceOf[Double], lon.asInstanceOf[Double])
+      new Bike(id.num.toInt, lat.num, lon.num)
   }
 }
 
 case class Bikes(bikes: Array[Bike])
+@JSExportTopLevel("BikeState")
+class BikeState
+@JSExportTopLevel("Rented")
+case object Rented extends BikeState
+@JSExportTopLevel("Available")
+case object Available extends BikeState
 
-@JSExport
+@JSExportTopLevel("Map")
 object Map extends {
-
 
   val mapOptions=LMapOptions.zoom(13).center((50.05, 19.95))
   val lmap = L.map("mapid", mapOptions)
-  var bikes: HashSet[Bike] = new HashSet[Bike]()
-  var markers: HashMap[Bike, Marker] = new HashMap[Bike, Marker]()
-
-  val leafOptions=IconOptions.iconSize((30,60))
-  val iconc=L.icon(leafOptions.iconUrl("img/bike.ico"))
+  var bikesAvailable: Set[Bike] = Set()
+  var bikesRented: Set[Bike] = Set()
+  var markers: HashMap[Bike, (Marker, LayerGroup)] = new HashMap[Bike, (Marker, LayerGroup)]()
+  val rented = L.layerGroup()
+  val available = L.layerGroup()
+  val leafOptions=IconOptions.iconSize((40,80))
+  val icon_rented=L.icon(leafOptions.iconUrl("img/bike.ico"))
   val icon=L.icon(leafOptions.iconUrl("img/bike-icon.png"))
-  val request = HttpRequest("https://wavelo-live.herokuapp.com/toupdate")
-  var lat = 50.05
-  var lon = 19.95
-  val marker = L.marker((lat, lon), MarkerOptions.icon(icon)).bindPopup(s"BikeID:\t${5}\nLatitude:\t${lat}\nLongitude:\t${lon}")
+  val requestRented = HttpRequest("https://wavelo-live.herokuapp.com/rented")
+  val requestAvailable = HttpRequest("https://wavelo-live.herokuapp.com/available")
+
   @JSExport
   def main(el: String): Unit = {
 
-    request.send().onComplete({
-      case res:Success[SimpleHttpResponse] =>
-        correct(res)
-      case e: Failure[SimpleHttpResponse] => println("Huston, we got a problem!")
-    })
-//    marker.addTo(lmap)
+    update(Available)
+    update(Rented)
+
     val tileLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZmFuY2VsbHUiLCJhIjoiY2oxMHRzZm5zMDAyMDMycndyaTZyYnp6NSJ9.AJ3owakJtFAJaaRuYB7Ukw",
       TileLayerOptions.id("mapbox.streets").maxZoom(19).attribution("""Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,
                                                                       |<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
                                                                       |Imagery © <a href="http://mapbox.com">Mapbox</a>""".stripMargin))
+    val basemaps = literal("base"->tileLayer)
+    val overlays = literal("Rented"->rented, "Available"->available)
+
+    L.control.layers(basemaps, overLays = overlays, ControlLayerOptions.collapsed(false).hideSingleBase(true)).addTo(lmap)
+
+    rented.addTo(lmap)
+    available.addTo(lmap)
     tileLayer.addTo(lmap)
 
-    L.control.scale().addTo(lmap)
 
-//    lmap
+    L.control.scale().addTo(lmap)
   }
 
   @JSExport
-  def update():Unit = {
+  def update(state: BikeState):Unit = {
+    val request = state match {
+      case Rented => requestRented
+      case _ => requestAvailable
+    }
     request.send().onComplete({
       case res:Success[SimpleHttpResponse] =>
-        correct(res)
-//        for (i <- 1 to 1000){
-//          lat += 0.001
-//          marker.setLatLng((lat, lon)).bindPopup(s"BikeID:\t${5}\nLatitude:\t${lat}\nLongitude:\t${lon}")
-//        }
-      case e: Failure[SimpleHttpResponse] => println("Huston, we got a problem!")
+        correct(res, state)
+      case e: Failure[SimpleHttpResponse] => println("Houston, we got a problem!")
     })
   }
 
-  def correct(res: Success[SimpleHttpResponse]): Unit = {
-    val newBikes: HashSet[Bike] = new HashSet() ++ default.read[Bikes](res.get.body).bikes
-    for (bike <- newBikes) {
-      markers.get(bike) match {
-        case Some(marker) =>
-
-          marker.bindPopup(s"BikeID:\t${bike.id}\nLatitude:\t${bike.latitude}\nLongitude:\t${bike.longitude}")
-          .setLatLng((bike.latitude, bike.longitude))
-        case None =>
-          val marker = L.marker((bike.latitude, bike.longitude), MarkerOptions.icon(icon))
-            .bindPopup(s"BikeID:\t${bike.id}\nLatitude:\t${bike.latitude}\nLongitude:\t${bike.longitude}").addTo(lmap)
-          markers = markers + ((bike, marker))
-      }
+  def changeState(bikes: Traversable[Bike], newState: BikeState): Unit ={
+    val (ico, newLayer) = newState match {
+      case Rented => (icon_rented, rented)
+      case _ => (icon, available)
     }
-    for (bike <- bikes -- newBikes) {
+    bikes.foreach(bike => {
       markers.get(bike) match {
-        case Some(marker) =>
-          lmap.removeLayer(marker)
-          markers = markers - bike
+        case Some((marker, layer)) =>
+          markers = markers + (bike -> ((marker, newLayer)))
+          layer.removeLayer(marker)
+          newLayer.addLayer(marker)
+          marker.setIcon(ico)
         case None => ()
       }
+    })
+  }
+
+  def correct(res: Success[SimpleHttpResponse], typ: BikeState): Unit = {
+    val newBikes = default.read[Bikes](res.get.body).bikes.toSet
+
+    bikesAvailable = typ match {
+      case Rented =>
+        (bikesAvailable | bikesRented) &~ newBikes
+      case _ =>
+        newBikes
     }
-    println("xD")
-    bikes = newBikes
+
+    bikesRented = typ match {
+      case Rented =>
+        newBikes
+      case _ =>
+        (bikesAvailable | bikesRented) &~ newBikes
+    }
+
+    val (dest, sec, ico) = typ match {
+      case Rented =>
+        changeState(bikesAvailable &~ newBikes, Available)
+        (rented, available, icon_rented)
+      case _ =>
+        changeState(bikesRented &~ newBikes, Rented)
+        (available, rented, icon)
+    }
+
+    for (bike <- newBikes) {
+      markers.get(bike) match {
+        case Some((marker, layer)) =>
+          if(layer != dest) {
+            markers = markers + (bike -> ((marker, dest)))
+            layer.removeLayer(marker)
+            dest.addLayer(marker)
+            marker.setIcon(ico)
+          }
+          marker.bindPopup(s"BikeID:\t${bike.id}<br>Latitude:\t${bike.latitude}<br>Longitude:\t${bike.longitude}")
+          .setLatLng((bike.latitude, bike.longitude))
+        case None =>
+          val marker = L.marker((bike.latitude, bike.longitude), MarkerOptions.icon(ico))
+            .bindPopup(s"BikeID:\t${bike.id}<br>Latitude:\t${bike.latitude}<br>Longitude:\t${bike.longitude}")
+          dest.addLayer(marker)
+          markers = markers + (bike -> ((marker, dest)))
+      }
+    }
   }
 }
